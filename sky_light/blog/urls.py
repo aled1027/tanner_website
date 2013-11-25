e31a955afaa1780410dbb0babb6ed61ae9dab99b
Alex Ledger <a.led1027@gmail.com>
2013-11-25 15:33:28 -0800

    changing over to other version

--- sky_light/blog/urls.py
+++ sky_light/blog/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import *
 from blog.views import *
 
 urlpatterns = patterns("",
-	(r'^article-list/$', ArticleListView, {}, "article_list"),
+	(r'^article-list/$', articleListView, {}, "article_list"),
 	(r'^article-detail/(?P<pk>\d+)/$', articleDetailView, {}, "article_detail"),
 	(r'^comment-detail/(?P<pk>\d+)/$', commentDetailView, {}, ""),
-	#(r'^article-detail/(?P<pk>\d+)/$', ArticleDetailView.as_view(), {}, "article_detail"),
-	(r'^article-detail/(?P<pk>\d+)/comment/(?P<comment_pk>\d+)/$', CommentDetailView.as_view(), {}, "comment_detail"),
-	(r'^create-comment/(?P<pk>\d+)/$', CommentFormView.as_view(), {}, "comment_form"),
-	(r'^create-rebuttal/(?P<pk>\d+)/$', RebuttFormView.as_view(), {}, "rebutt_form"),
+	(r'^article-detail/(?P<pk>\d+)/comment/(?P<comment_pk>\d+)/$', commentDetailView, {}, "comment_detail"),
+	(r'^create-comment/(?P<pk>\d+)/$', commentFormView, {}, "comment_form"),
+	(r'^create-rebuttal/(?P<pk>\d+)/$', rebuttFormView, {}, "rebutt_form"),
 )
--- sky_light/blog/views.py
+++ sky_light/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.core.context_processors import csrf
 import time
 from calendar import month_name
 from blog.models import *
@@ -11,16 +12,13 @@ from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from django.http import Http404
 
-def ArticleListView(request):
-	try:
-		arts = Article.objects.all()
-	except Article.DoesNotExist:
-		raise Http404
-	return render(request, "list.html", {"articles": arts})
+def articleListView(request):
+	args = {}
+	#args.update(csrf(request))
+	args['articles'] = Article.objects.all()
+	return render(request, "list.html", args)
 
-@login_required
 def articleDetailView(request, pk):  #successful!
-
 	try: 
 		a = Article.objects.get(pk=pk)
 	except Article.DoesNotExist:
@@ -28,18 +26,18 @@ def articleDetailView(request, pk):  #successful!
 	return render(request, "detail.html", {"article": a})
 
 def articleCreateView(request, a_id=None):
- 	if a_id:
-        a = Article.objects.get(pk=a_id)
-    else:
-        a = None
-    if request.method == 'POST':
-        form = ArticleForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save
-            return HttpResponseRedirect(reverse('article-detail', args=[form.article.id]))
-    else:
-        form = MembershipForm(instance=a)
-        return render(request, 'article_form.html', {'form': form})
+	if a_id:
+		a = Article.objects.get(pk=a_id)
+	else:
+		a = None
+	if request.method == 'POST':
+		form = ArticleForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save
+			return HttpResponseRedirect(reverse('article-detail', args=[form.article.id]))
+   	else:
+		form = MembershipForm(instance=a)
+		return render(request, 'article_form.html', {'form': form})
 
 #not sure if this works
 def commentDetailView(request, pk):
@@ -53,29 +51,39 @@ def commentDetailView(request, pk):
 
 def commentFormView(request, c_id=None):
 	if c_id:
-        c = Comment.objects.get(pk=c_id)
-    else:
-        c = None
-    if request.method == 'POST':
-        form = CommentForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save
-            return HttpResponseRedirect(reverse('comment-detail', args=[form.comment.id]))
-    else:
-        form = MembershipForm(instance=c)
-        return render(request, 'article_form.html', {'form': form})
+		c = Comment.objects.get(pk=c_id)
+	else:
+		c = None
+	if request.method == 'POST':
+		form = CommentForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save
+			return HttpResponseRedirect(reverse('comment-detail', args=[form.comment.id]))
+	else:
+		form = MembershipForm(instance=c)
+		return render(request, 'article_form.html', {'form': form})
 
 def rebuttFormView(request, r_id=None):
 	if r_id:
-        r = Comment.objects.get(pk=r_id)
-    else:
-        r = None
-    if request.method == 'POST':
-        form = CommentForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save
-            return HttpResponseRedirect(reverse('comment-detail', args=[form.comment.id]))
-    else:
-        form = MembershipForm(instance=r)
-        return render(request, 'article_form.html', {'form': form})
+		r = Comment.objects.get(pk=r_id)
+	else:
+		r = None
+	if request.method == 'POST':
+		form = CommentForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save
+			return HttpResponseRedirect(reverse('comment-detail', args=[form.comment.id]))
+	else:
+		form = MembershipForm(instance=r)
+		return render(request, 'article_form.html', {'form': form})
+
+#def search_articles(request):
+#	if request.method == 'POST':
+#		search_text = request.Post['search_text']
+#	else: 
+#		search_text = ''
+#	articles = Article.objects.filter(title__contains==search_text)
+#	return render(request, 'ajax_search.html', {'articles': articles})
+#
+
 
